a6edef2492e5987ddb7eb542bea5e7f3edf05877
DonghyunKim <leok0909@snu.ac.kr>
2022-08-21 20:58:27 +0900

    basic server, client implementation

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+
+PORT = 15052
+SERVER = '127.0.0.1'
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+ADDR = (SERVER, PORT)
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+send(DISCONNECT_MESSAGE)
---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+import threading
+
+PORT = 15052
+# Todo: Temporally hard code local host.
+# SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = '127.0.0.1'
+ADDR = (SERVER, PORT)
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("MSG received".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+if __name__ == '__main__':
+    print("[STARTING] server is starting")
+    start()
